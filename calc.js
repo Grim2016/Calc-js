ccc6958876921b83309527d3a99532bdb29adc50
Grim2016 <brage.ovrebo@outlook.com>
2022-02-23 21:50:29 +0100

    Lagt til javascripten

--- calc.js
+++ calc.js
@@ -1,2 +1,12 @@
-var text = document.getElementById("tallVisning")
-text.innerHTML = "Halla"
+var text = document.getElementById("tallVisning");
+text.innerHTML = "Halla";
+
+var num1;
+var num2;
+var operator;
+var button_array = [];
+for(i = 0; i < 10; i++) {
+    button_array[i] = document.getElementById("b"+String(i))
+}
+for(i = 0;)
+console.log(button_array)
