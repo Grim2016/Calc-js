11e5b13ac8f8c240173b15b5c800cf7a82ad1fcc
Grim2016 <brage.ovrebo@outlook.com>
2022-02-23 21:59:18 +0100

    Added concatinate number function

--- calc.js
+++ calc.js
@@ -5,8 +5,10 @@ var num1;
 var num2;
 var operator;
 var button_array = [];
+
+function concatinateNumber(number) {}
 for(i = 0; i < 10; i++) {
     button_array[i] = document.getElementById("b"+String(i))
+    button_array[i].addEventListener("click", concatinateNumber(i))
 }
-for(i = 0;)
 console.log(button_array)
