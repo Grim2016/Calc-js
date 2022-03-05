50be5bd1eee7a01e2347bf4a6c4ea674700e4c8a
Grim2016 <brage.ovrebo@outlook.com>
2022-03-05 22:38:42 +0100

    La til evnen til å regne potenser

--- calc.js
+++ calc.js
@@ -1,5 +1,8 @@
 var tekst = document.getElementById("tallVisningsDiv");
-tekst.innerHTML = " ";
+var html = {
+    tall1: document.getElementById("tall1"),
+    tall2: document.getElementById("tall2")
+};
 var tall = {tall1: 0, tall2: 0}
 var operator;
 var tallKnappArray = [];
@@ -9,25 +12,31 @@ function log() {
     console.log(`Operator: ${operator}\nTall 1: ${tall.tall1}\nTall 2: ${tall.tall2}`);
 }
 
+function settIndreTall () {
+    html.tall1.innerHTML = String(tall.tall1).replace(".",",");
+    html.tall2.innerHTML = String(tall.tall2).replace(".",",");
+}
+
 //Setter alle tall knappene i en array og binder en funksjon til dem
 for (i = 0; i < 10; i++) {
     tallKnappArray[i] = document.getElementById("k"+String(i));
     tallKnappArray[i].onclick = function () {
         if(tallSomSkalVises == 1) {
             tall.tall2 = 0;
-            tall.tall1 = Number(String(tall.tall1) + this.innerHTML);
+            tall.tall1 = String(tall.tall1) + this.innerHTML;
             if(String(tall.tall1)[0] == 0 && String(tall.tall1)[1] != ".") {
                 let tall1String = String(tall.tall1);
                 tall1String[0] = "";
-                tall.tall2 = Number(tall1String);
+                tall.tall1 = tall1String;
             }
         }
         else {
-            tall.tall2 = Number(String(tall.tall2) + this.innerHTML);
+            tall.tall2 = String(tall.tall2) + this.innerHTML;
             if(String(tall.tall2)[0] == 0 && String(tall.tall2)[1] != ".") {
                 let tall2String = String(tall.tall2);
+                console.log("Boi")
                 tall2String[0] = "";
-                tall.tall2 = Number(tall2String);
+                tall.tall2 = tall2String;
             }
         }
         if(tallSomSkalVises == 1) {
@@ -36,6 +45,7 @@ for (i = 0; i < 10; i++) {
         else {
             tekst.innerHTML = String(tall.tall2).replace(".",",");
         }
+        settIndreTall()
         log();
     }
 }
@@ -43,7 +53,16 @@ for (i = 0; i < 10; i++) {
 
 
 function regneUt() {
-    log()
+    if(String(tall.tall1).includes("^")) {
+        let tall1String = String(tall.tall1)
+        let tall1Eksponent = Number(tall1String.split("^")[1])
+        tall.tall1 = Number(tall1String.split("^")[0])**tall1Eksponent
+    }
+    if(String(tall.tall2).includes("^")) {
+        let tall1String = String(tall.tall1)
+        let tall1Eksponent = Number(tall1String.split("^")[1])
+        tall.tall2 = Number(tall1String.split("^")[0])**tall1Eksponent
+    }
     switch (operator) {
         case "Pluss":
             tall.tall1 += Number(tall.tall2);
@@ -60,6 +79,7 @@ function regneUt() {
     }
     tallSomSkalVises = 1;
     tekst.innerHTML = String(tall.tall1).replace(".",",");
+    settIndreTall()
     log();
 }
 
@@ -91,12 +111,24 @@ class AnnenKnapp {
     constructor (Navn, Funksjon) {
         this.html = document.getElementById("k"+Navn);
         this.html.onclick = function () {
+            
+            if(String(tall.tall1).includes("^")) {
+                let tall1String = String(tall.tall1)
+                let tall1Eksponent = Number(tall1String.split("^")[1])
+                tall.tall1 = Number(tall1String.split("^")[0])**tall1Eksponent
+            }
+            if(String(tall.tall2).includes("^")) {
+                let tall1String = String(tall.tall1)
+                let tall1Eksponent = Number(tall1String.split("^")[1])
+                tall.tall2 = Number(tall1String.split("^")[0])**tall1Eksponent
+            }
             if(tallSomSkalVises == 1) {
                 Funksjon("tall1");
             }
             else {
                 Funksjon("tall2");
             }
+            settIndreTall()
         }
     }
 }
@@ -137,7 +169,9 @@ const kommaKnapp = new AnnenKnapp("Komma", function (index) {
 const invertererKnapp = new AnnenKnapp("Inverterer", function (index) {
     tall[index] = tall[index] - tall[index]*2;
     tekst.innerHTML = String(tall[index]).replace(".",",");
-
+})
+const eksponentKnapp = new AnnenKnapp("Potens", function (index) {
+    tall[index] = String(tall[index]) +"^"
 })
 
 console.log(tallKnappArray);
